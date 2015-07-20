310fdbc6717b9e0d3a3fddb714d60cfd0b8fdf9d
altroz <nhb913@outlook.com>
2015-07-20 11:10:45 -0700

    SqlQueueTest

---
+++ src/generics/classGenerics/SqlQueueTest.java
@@ -0,0 +1,11 @@
+package generics.classGenerics;
+
+public class SqlQueueTest {
+	public static void main(String[] args) {
+		SqlQueue sqlQ = new SqlQueue();
+		sqlQ.addInQueue("Harrison");
+		sqlQ.addInQueue("Alex");
+		sqlQ.addInQueue("Nazmul");
+		sqlQ.addInQueue("Lennon");
+	}
+}
