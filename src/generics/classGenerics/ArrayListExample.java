238142a05db81cae339d224a52c71c84914a9263
altroz <nhb913@outlook.com>
2015-07-20 08:07:01 -0700

    Initaliating the Iterator class to iterate through the list.

--- src/generics/classGenerics/ArrayListExample.java
+++ src/generics/classGenerics/ArrayListExample.java
@@ -1,16 +1,19 @@
 package generics.classGenerics;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListExample {
-	public void languageSelection(){
-	ArrayList<String>languageList = new ArrayList<String>();
-	languageList.add("C");
-	languageList.add("C++");
-	languageList.add("Java");
-	
-	for(String language: languageList){
-		System.out.println(language);
-	}
+	public void languageSelection() {
+		ArrayList<String> languageList = new ArrayList<String>();
+		languageList.add("C");
+		languageList.add("C++");
+		languageList.add("Java");
+
+		for (Iterator<String> languageItr = languageList.iterator(); languageItr
+				.hasNext();) {
+			String language = languageItr.next();
+			System.out.println(language);
+		}
 	}
 }
