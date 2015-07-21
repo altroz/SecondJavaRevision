8aab5fc20b427366083f68f6f1cf16844ee496ed
altroz <nhb913@outlook.com>
2015-07-21 16:53:25 -0700

    AssertionExample1 class displays the use of "simple" assertion

---
+++ src/assertion/one/AssertionExample1.java
@@ -0,0 +1,15 @@
+package assertion.one;
+
+public class AssertionExample1 {
+	public static void main(String[] args) {
+		int i = -10;
+		if (i < 0) {
+			// if negative value, convert into positive value
+			i = -i;
+		}
+		System.out.println("The value of i is: " + i);
+		// at this point the assumption is that i cannot be negative;
+		// assert this condition since its an assumption that will always hold.
+		assert (i >= 0) : "impossible: i is negative. ";
+	}
+}
