23a4a7bcdefccf9e5c34e04096c599a332aad219
altroz <nhb913@outlook.com>
2015-07-21 17:53:32 -0700

    Chained Exception

---
+++ src/exception/one/ChainException.java
@@ -0,0 +1,27 @@
+package exception.one;
+
+public class ChainException {
+	public static void foo() {
+		try {
+			String[] str = { "foo" };
+			System.out.println("About to throw ArrayIndexOutOfBoundsException");
+			// following statement haas out-of-bound access
+			String functionName = str[10];
+		} catch (ArrayIndexOutOfBoundsException oob) {
+			System.out
+					.println("Wrapping ArrayIndexOutOfBoundException into a RuntimeException");
+			throw new RuntimeException(oob);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			foo();
+		} catch (Exception re) {
+			System.out.println("The caught exception in main is: "
+					+ re.getClass());
+			System.out.println("The cause of the exception is: "
+					+ re.getCause());
+		}
+	}
+}
