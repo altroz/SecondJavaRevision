3c25791d69cb3ac39e0b846d03ee860677a1a7c7
altroz <nhb913@outlook.com>
2015-07-25 15:03:59 -0700

    parseing strings with .split method.

---
+++ src/stringProcessing/one/ParseString1.java
@@ -0,0 +1,13 @@
+package stringProcessing.one;
+
+public class ParseString1 {
+	public static void main(String[] args) {
+		String quote = "Never lend books" + "- nobody ever returns " + "them!";
+
+		String[] words = quote.split(" ");
+
+		for (String word : words) {
+			System.out.println(word);
+		}
+	}
+}
