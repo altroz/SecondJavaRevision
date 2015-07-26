34428973090c74100e0c259579fe7f5fb7500c50
altroz <nhb913@outlook.com>
2015-07-26 10:51:44 -0700

    SearchString test 1

---
+++ src/stringProcessing/one/SearchString1.java
@@ -0,0 +1,8 @@
+package stringProcessing.one;
+
+public class SearchString1 {
+	public static void main(String[] args) {
+		String str = "OCPJP";
+		System.out.println("Character J occurs at index " + (str.indexOf("J")+1)+ "th position");
+	}
+}
