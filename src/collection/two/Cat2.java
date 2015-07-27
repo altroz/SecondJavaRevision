512934e880adc3fe429ed903e1f90631748b6bcc
altroz <nhb913@outlook.com>
2015-07-26 17:36:38 -0700

    Cat2 class created

---
+++ src/collection/two/Cat2.java
@@ -0,0 +1,13 @@
+package collection.two;
+
+public class Cat2 {
+	private int catNumber;
+
+	Cat2(int i) {
+		catNumber = i;
+	}
+
+	void print() {
+		System.out.println("Cat number " + catNumber);
+	}
+}
