248d9a1a5631580f15ced1f6ce0cfd1bc6582a59
altroz <nhb913@outlook.com>
2015-07-26 17:40:47 -0700

    Hamster class created.

---
+++ src/collection/two/Hamster.java
@@ -0,0 +1,14 @@
+package collection.two;
+
+public class Hamster {
+	private int hamsterNumber;
+
+	Hamster(int i) {
+		hamsterNumber = i;
+
+	}
+
+	public String toString() {
+		return "This is Hamster  #: " + hamsterNumber;
+	}
+}
